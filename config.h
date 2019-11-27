5b4ac0624275b86c149c26ae176012b680d6c1c8
Ryan Vaughan <ryan@j0b.net>
2019-11-27 16:43:51 -0600

    launch st launch dwmblocks

--- config.h
+++ config.h
@@ -95,6 +95,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -156,6 +157,8 @@ static Key keys[] = {
 	/* { MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } }, */
 	/* { MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } }, */
 	/* { MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } }, */
+	{ MODKEY|ShiftMask,		XK_Return,	 spawn,     {.v = termcmd } },
+	{ MODKEY,							XK_p,				 spawn,			{.v = dmenucmd } },
 };
 
 /* button definitions */
